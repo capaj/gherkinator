bf0e69706f6bec0746073cd9ef5b99f7d66d483e
Jiri Spac <jiri.spac.contractor@avg.com>
2014-10-22 11:44:48 +0200

    removed obsolet line

--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 var analyzer = require('../gherkinator').analyzer;
 var should = require('should');
 
-//gherkinator.file('../../features/apps/AV/remote/state_v3.feature');
-
 describe('analyzer outputs with test feature files', function() {
 	it('should return an array of size 6 for one basic file', function() {
 		var keys = Object.keys(analyzer.file('./test/test_features/basic.feature'));
